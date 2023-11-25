580da567a7e20d72b2da33433bfe388241887170
Doc Viper <91398017+doctor-viper@users.noreply.github.com>
2023-11-24 20:55:32 -0500

    small updates

--- inc/functions.php
+++ inc/functions.php
@@ -7,9 +7,9 @@
         "cigar v1.0",
         "cowoboy-hat v6.6.6",
         "movies v1.2.3",
-        "tv-guide v3.7"
+        "viper-guide v3.7",
+        "sfx_pkg v8.6.7"
     );
-
 
     // Gets a random amount of packages
     function get_random_packages($num = 3) {
@@ -17,6 +17,5 @@
         shuffle($vipos_packages);
         return array_slice($vipos_packages, 0, $num);
     }
-
 
 ?>
