eda9c1e4635191bea3529ff596409f44a9e6d068
Doc Viper <doc@viperverse.tv>
2025-01-04 15:33:02 -0500

    markup and style updates

--- inc/footer.php
+++ inc/footer.php
@@ -2,7 +2,5 @@
       <p><small>&copy; 2023 Your Name Here. All Rights Reserved.</small></p>
     </footer>
 
-  </div>
-
 </body>
 </html>
--- inc/header.php
+++ inc/header.php
@@ -63,8 +63,6 @@
 
 <body>
 
-<div>
-
   <header>
     <div>
     
