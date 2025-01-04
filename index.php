42b25936c89fc1987a73961c2d4b374c50f52a89
Doc Viper <doc@viperverse.tv>
2025-01-03 23:10:04 -0500

    No more terminal window

--- index.php
+++ index.php
@@ -1,69 +1,81 @@
 <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
 
+  <div class="box">
+    <div class="sun"></div>
+    <div class="sunglow"></div>
+    <div class="sunfirstlayer"></div>
+    <div class="sunsecondlayer"></div>
+    <div class="sunthirdlayer"></div>
+    <div class="sunfourthlayer"></div>
+    <div class="sunfifthlayer"></div>
+    <div class="sunsixthlayer"></div>
+    <div class="sunseventhlayer"></div>
+    <div class="suneighthlayer"></div>
+    <div class="sunninthlayer"></div>
+  </div>
+
   <main class="clearfix">
 
-    <section class="hero-image-and-title">
-
-      <div class="doc-terminal">
-
-        <div class="fake-toolbar">
-          <div class="fake-button viperverse y-btn"></div>
-          <div class="fake-button viperverse fake-yellow"></div>
-          <div class="fake-button viperverse fake-green n-btn"></div>
-        </div>
         
-        <h2>welcome to the</h2>
+    <h2>welcome to the</h2>
 
-<p>&nbsp;_&nbsp;&nbsp;&nbsp;&nbsp;__________&nbsp;&nbsp;__________&nbsp;_&nbsp;&nbsp;&nbsp;&nbsp;____________&nbsp;&nbsp;_____&nbsp;______</p>
-<p>|&nbsp;|&nbsp;&nbsp;/&nbsp;/&nbsp;&nbsp;_/&nbsp;__&nbsp;\/&nbsp;____/&nbsp;__&nbsp;\&nbsp;|&nbsp;&nbsp;/&nbsp;/&nbsp;____/&nbsp;__&nbsp;\/&nbsp;___//&nbsp;____/</p>
-<p>|&nbsp;|&nbsp;/&nbsp;//&nbsp;//&nbsp;/_/&nbsp;/&nbsp;__/&nbsp;/&nbsp;/_/&nbsp;/&nbsp;|&nbsp;/&nbsp;/&nbsp;__/&nbsp;/&nbsp;/_/&nbsp;/\__&nbsp;\/&nbsp;__/</p>
-<p>|&nbsp;|/&nbsp;//&nbsp;//&nbsp;____/&nbsp;/___/&nbsp;_,&nbsp;_/|&nbsp;|/&nbsp;/&nbsp;/___/&nbsp;_,&nbsp;_/___/&nbsp;/&nbsp;/___</p>
-<p>|___/___/_/&nbsp;&nbsp;&nbsp;/_____/_/&nbsp;|_|&nbsp;|___/_____/_/&nbsp;|_|/____/_____/</p>
+    <p>&nbsp;_&nbsp;&nbsp;&nbsp;&nbsp;__________&nbsp;&nbsp;__________&nbsp;_&nbsp;&nbsp;&nbsp;&nbsp;____________&nbsp;&nbsp;_____&nbsp;______</p>
+    <p>|&nbsp;|&nbsp;&nbsp;/&nbsp;/&nbsp;&nbsp;_/&nbsp;__&nbsp;\/&nbsp;____/&nbsp;__&nbsp;\&nbsp;|&nbsp;&nbsp;/&nbsp;/&nbsp;____/&nbsp;__&nbsp;\/&nbsp;___//&nbsp;____/</p>
+    <p>|&nbsp;|&nbsp;/&nbsp;//&nbsp;//&nbsp;/_/&nbsp;/&nbsp;__/&nbsp;/&nbsp;/_/&nbsp;/&nbsp;|&nbsp;/&nbsp;/&nbsp;__/&nbsp;/&nbsp;/_/&nbsp;/\__&nbsp;\/&nbsp;__/</p>
+    <p>|&nbsp;|/&nbsp;//&nbsp;//&nbsp;____/&nbsp;/___/&nbsp;_,&nbsp;_/|&nbsp;|/&nbsp;/&nbsp;/___/&nbsp;_,&nbsp;_/___/&nbsp;/&nbsp;/___</p>
+    <p>|___/___/_/&nbsp;&nbsp;&nbsp;/_____/_/&nbsp;|_|&nbsp;|___/_____/_/&nbsp;|_|/____/_____/</p>
 
 
-        
-        <svg>
+    
+
+
+    <div id="svg-container">
       
-          <!-- VERTICAL LINES -->
-          <line x1="48%" y1="0" x2="40%" y2="100%" />
-          <line x1="52%" y1="0" x2="60%" y2="100%" />
+      <svg>
+    
+        <!-- VERTICAL LINES -->
+        <line x1="48%" y1="0" x2="40%" y2="100%" />
+        <line x1="52%" y1="0" x2="60%" y2="100%" />
 
-          <line x1="44%" y1="0" x2="20%" y2="100%" />
-          <line x1="56%" y1="0" x2="80%" y2="100%" />
+        <line x1="44%" y1="0" x2="20%" y2="100%" />
+        <line x1="56%" y1="0" x2="80%" y2="100%" />
 
-          <line x1="40%" y1="0" x2="0%" y2="100%" />
-          <line x1="60%" y1="0" x2="100%" y2="100%" />
+        <line x1="40%" y1="0" x2="0%" y2="100%" />
+        <line x1="60%" y1="0" x2="100%" y2="100%" />
 
-          <line x1="36%" y1="0" x2="0%" y2="60%" />
-          <line x1="64%" y1="0" x2="100%" y2="60%" />
+        <line x1="36%" y1="0" x2="0%" y2="60%" />
+        <line x1="64%" y1="0" x2="100%" y2="60%" />
 
-          <line x1="32%" y1="0" x2="0%" y2="30%" />
-          <line x1="68%" y1="0" x2="100%" y2="30%" />
+        <line x1="32%" y1="0" x2="0%" y2="30%" />
+        <line x1="68%" y1="0" x2="100%" y2="30%" />
 
-          <line x1="28%" y1="0" x2="0%" y2="10%" />
-          <line x1="72%" y1="0" x2="100%" y2="10%" />
+        <line x1="28%" y1="0" x2="0%" y2="10%" />
+        <line x1="72%" y1="0" x2="100%" y2="10%" />
 
-          <line x1="24%" y1="0" x2="0%" y2="5%" />
-          <line x1="76%" y1="0" x2="100%" y2="5%" />
+        <line x1="24%" y1="0" x2="0%" y2="5%" />
+        <line x1="76%" y1="0" x2="100%" y2="5%" />
 
-          <line x1="20%" y1="0" x2="0%" y2="2.5%" />
-          <line x1="80%" y1="0" x2="100%" y2="2.5%" />
+        <line x1="20%" y1="0" x2="0%" y2="2.5%" />
+        <line x1="80%" y1="0" x2="100%" y2="2.5%" />
 
-          <!-- HORIZON -->
-          <line class="horizon" x1="0" y1="0" x2="100%" y2="0" />
+        <!-- HORIZON -->
+        <line class="horizon" x1="0" y1="0" x2="100%" y2="0" />
 
-          <!-- MOVING LINES -->
-          <line class="moving-line" x1="0" y1="0" x2="100%" y2="0" />      
-          <line class="moving-line two" x1="0" y1="0" x2="100%" y2="0" />
-          <line class="moving-line three" x1="0" y1="0" x2="100%" y2="0" />
-          <line class="moving-line four" x1="0" y1="0" x2="100%" y2="0" />
-          <line class="moving-line five" x1="0" y1="0" x2="100%" y2="0" />
+        <!-- MOVING LINES -->
+        <line class="moving-line" x1="0" y1="0" x2="100%" y2="0" />      
+        <line class="moving-line two" x1="0" y1="0" x2="100%" y2="0" />
+        <line class="moving-line three" x1="0" y1="0" x2="100%" y2="0" />
+        <line class="moving-line four" x1="0" y1="0" x2="100%" y2="0" />
+        <line class="moving-line five" x1="0" y1="0" x2="100%" y2="0" />
+        <line class="moving-line six" x1="0" y1="0" x2="100%" y2="0" />
+        <line class="moving-line seven" x1="0" y1="0" x2="100%" y2="0" />
+        <line class="moving-line eight" x1="0" y1="0" x2="100%" y2="0" />
+        <line class="moving-line nine" x1="0" y1="0" x2="100%" y2="0" />
+        <line class="moving-line ten" x1="0" y1="0" x2="100%" y2="0" />        
 
-        </svg>
+      </svg>
 
-      </div>
-      
-    </section>
+    </div>
 
   </main>
 
