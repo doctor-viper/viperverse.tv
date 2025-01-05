930a798d9ddf8bfbf6317f223e7a88cf96105501
Doc Viper <91398017+doctor-viper@users.noreply.github.com>
2025-01-04 21:40:57 -0500

    remove refs to jquery and just use vanilla

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,63 +1,28 @@
 /**
- * Document.ready function
- * 
+ * ALL SYSTEMS NOMINAL.
  */
-$( document ).ready(function() {
-
-  get_random_packages(true);
-
-});
-
-
-
-/**
- * Gets random ficticious Viperverse 
- * themed NPM package descriptions 
- *
- */
-function get_random_packages(console = false) {
-  $.ajax({
-    url:      '/api/v1/packages/index.php',
-    type:     'POST',
-    dataType: 'json',
-    success:  function(data) {
-      if(console) {
-        viperverse_console(data);
-      }
-    },
-    error: function (xhr, ajaxOptions, thrownError) {
-      console.log(xhr.status);
-      console.log(thrownError);
-      console.log(xhr.responseText);
-    }
-  });
-
-}
+viperverse_console();
 
 
 /**
  * Default console gibberish, spit out in monospace
  * doesn't do anything, just silly fun
- * 
  */
-function viperverse_console(data) {
+function viperverse_console() {
 
 console.log(`%c
-WELCOME TO THE
+LOGGING INTO THE . . . 
  _    __________  __________ _    ____________  _____ ______
 | |  / /  _/ __ \\/ ____/ __ \\ |  / / ____/ __ \\/ ___// ____/
 | | / // // /_/ / __/ / /_/ / | / / __/ / /_/ /\\__ \\/ __/   
 | |/ // // ____/ /___/ _, _/| |/ / /___/ _, _/___/ / /___   
 |___/___/_/   /_____/_/ |_| |___/_____/_/ |_|/____/_____/ 
 
-KNOWLEDGE IS POWER - FOR REAL.
+* REACTOR . . . ONLINE
+* SENSORS . . . ONLINE
+* SYSTEMS . . . ONLINE
 
-LOADING: 
-- ${data[0]}
-- ${data[1]}
-- ${data[2]}
-- ${data[3]}
-- ${data[4]}
+KNOWLEDGE IS POWER - FOR REAL.
 
 viper:\\>`,
 `font-family:monospace`
