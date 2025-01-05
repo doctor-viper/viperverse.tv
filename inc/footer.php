a951c0f8dce54d2447f01bbc2a517eea76959cfe
Doc Viper <91398017+doctor-viper@users.noreply.github.com>
2025-01-05 12:58:12 -0500

    updated comments

--- inc/footer.php
+++ inc/footer.php
@@ -1,10 +1,13 @@
-    <footer>
-      <span class="text">Copyright &copy; <?php echo date("Y"); ?></span>
-      <span class="spacer">&nbsp;&nbsp;//&nbsp;&nbsp;</span>
-      <span class="text"><a target="_blank" href="https://github.com/doctor-viper/viperverse.tv">viperverse.tv</a></span>
-      <span class="spacer">&nbsp;&nbsp;//&nbsp;&nbsp;</span>
-      <span class="text">all rights reserved</span>
-    </footer>
+  <!-- -------------------------------- -->
+  <!-- Site Footer                      -->
+  <!-- -------------------------------- -->
+  <footer>
+    <span class="text">Copyright &copy; <?php echo date("Y"); ?></span>
+    <span class="spacer">&nbsp;&nbsp;//&nbsp;&nbsp;</span>
+    <span class="text"><a target="_blank" href="https://github.com/doctor-viper/viperverse.tv">viperverse.tv</a></span>
+    <span class="spacer">&nbsp;&nbsp;//&nbsp;&nbsp;</span>
+    <span class="text">all rights reserved</span>
+  </footer>
 
 </body>
 </html>
