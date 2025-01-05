7fadb3701f11966e20a06a5652eb1eacac04a26c
Doc Viper <91398017+doctor-viper@users.noreply.github.com>
2025-01-04 23:10:33 -0500

    remove tracking stuff for now

--- inc/header.php
+++ inc/header.php
@@ -64,16 +64,6 @@
 
 </head>
 
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-W4429NHX4Z"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-W4429NHX4Z');
-</script>
-
 <body>
 
   <header>
