a990d22ac4cc2f7b15baa2ffcc263a0ad0e284fb
Doc Viper <91398017+doctor-viper@users.noreply.github.com>
2025-01-05 18:37:49 -0500

    more trailing slash removals on void elements

--- inc/header.php
+++ inc/header.php
@@ -33,7 +33,7 @@
   <meta name="author" content="Doc Viper">
   <meta name="copyright" content="<?php echo date('Y')?> - viperverse.tv">
 
-  <link rel="stylesheet" href="/assets/css/style.css" type="text/css" />
+  <link rel="stylesheet" href="/assets/css/style.css" type="text/css">
 
   <link rel="apple-touch-icon" sizes="180x180" href="/assets/img/apple-touch-icon.png">
   <link rel="icon" type="image/png" sizes="32x32" href="/assets/img/favicon-32x32.png">
@@ -74,7 +74,7 @@
         <a href="/">VIPERVERSE</a>
       </h1>
 
-      <img class="sun" src="/assets/img/viper_pfp_optimized.png" alt="SUN" />
+      <img class="sun" src="/assets/img/viper_pfp_optimized.png" alt="SUN">
 
       <nav>
         <ul>
