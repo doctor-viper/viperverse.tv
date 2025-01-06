6ff1438f2dd4c98cb44e13e3cc2548c653afced1
Doc Viper <91398017+doctor-viper@users.noreply.github.com>
2025-01-05 20:12:38 -0500

    more indents
    okay for real done

--- api/v1/packages/index.php
+++ api/v1/packages/index.php
@@ -1,8 +1,8 @@
 <?php 
-    // Set Content Type ( JSON )
-    header('Content-Type: application/json; charset=utf-8');
-    // Include the functions file
-    require_once($_SERVER['DOCUMENT_ROOT'].'/inc/functions.php');
-    // Echo the returned contents of the function
-    echo json_encode(get_random_packages());
+  // Set Content Type ( JSON )
+  header('Content-Type: application/json; charset=utf-8');
+  // Include the functions file
+  require_once($_SERVER['DOCUMENT_ROOT'].'/inc/functions.php');
+  // Echo the returned contents of the function
+  echo json_encode(get_random_packages());
 ?>
